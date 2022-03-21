bb87f06e841280b1c3805c53dffa035c6aeff7cc
regukiko <95239827+regukiko@users.noreply.github.com>
2022-03-21 18:15:31 +0100

    Update game menu.py

--- game menu.py
+++ game menu.py
@@ -4,13 +4,15 @@ import gallow
 import stone
 import number
 import stones_100
+import hero_adventure
 while True:
     print("1.Word jumble")
     print("2.Gallow")
     print("3.Stone Paper..")
     print("4.Guess the number")
     print("5.Stones")
-    print("6.Leave")
+    print("6.Hero Adventure")
+    print("7.Leave")
     a=input()
     if a=="1":
         jumble.jumble()
@@ -21,6 +23,9 @@ while True:
     elif a=="4":
         number.number()
     elif a=="5":
-        stones_100.play()
+        language=input("english-deutsch")
+        stones_100.play(language)
     elif a=="6":
+        hero_adventure.game()
+    elif a=="7":
         break
